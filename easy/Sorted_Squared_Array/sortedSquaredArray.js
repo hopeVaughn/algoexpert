3fece184cdd5d26b66afb7bde48aa735fe5a17ed
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-24 10:26:01 -0800

    add sorted array challenge

---
+++ easy/Sorted_Squared_Array/sortedSquaredArray.js
@@ -0,0 +1,19 @@
+/*
+Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+Tip: While the integers in the input array are sorted in increasing order, their squares won't necessarily be as well, because of the possible presence of negative numbers
+
+Sample input:
+array = [1,2,3,4,5,6,8,9]
+
+sample output:
+[1,4,9,25,36,64,81]
+*/
+const array = [1, 2, 3, 4, 5, 6, 8, 9]
+function sortedSquaredArray(array) {
+  // Write your code here.
+  return [];
+}
+
+console.log(sortedSquaredArray(array));
+
