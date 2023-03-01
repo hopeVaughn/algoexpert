c2d4f72d0a9894e7c0efa6e30cef04827288c6fe
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-01 11:42:49 -0800

    add iterative & recursive solutions bst challenge

---
+++ easy/Find_Closest_Value_In_BST/findClosestValueInBstIterative.js
@@ -0,0 +1,67 @@
+/*
+Write a function that takes in a Binary Search Tree(BST) and a target integer value and returns the closest value to that target value contained in the BST.
+
+You can assume that there will only be one closest value.
+
+Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children nodes are either valid BST nodes themselves or None/null. 
+
+Sample Input:
+tree =  10
+        /\
+      5   15
+     /\   /\
+    2  5 13 22
+  /       \
+ 1        14
+
+target: 12
+
+
+*/
+
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  let closest = Infinity;
+  return findClosestValueInBstHelper(tree, target, closest = Infinity)
+  console.log(tree);
+}
+function findClosestValueInBstHelper(tree, target, closest) {
+  let currentNode = tree;
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value
+    }
+    if (target < currentNode.value) {
+      currentNode = currentNode.left
+    } else if (target > currentNode.value) {
+      currentNode = currentNode.right
+    } else {
+      break;
+    }
+  }
+  return closest;
+}
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const root = new BST(10);
+root.left = new BST(5);
+root.left.left = new BST(2);
+root.left.left.left = new BST(1);
+root.left.right = new BST(5);
+root.right = new BST(15);
+root.right.left = new BST(13);
+root.right.left.right = new BST(14);
+root.right.right = new BST(22);
+
+console.log(findClosestValueInBst(root, 12));
+// average time: O(log(n))
+// worst case time: O(n)  *if we only have one branch in our tree
+// if solved iteratively space: O(1)
+// worst case space: O(n) 
--- easy/Find_Closest_Value_In_BST/findClosestValueInBstRecursive.js
+++ easy/Find_Closest_Value_In_BST/findClosestValueInBstRecursive.js
@@ -22,9 +22,21 @@ target: 12
 function findClosestValueInBst(tree, target) {
   // Write your code here.
   let closest = Infinity;
+  return findClosestValueInBstHelper(tree, target, closest = Infinity)
   console.log(tree);
 }
-
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (tree === null) return closest
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value
+  }
+  if (target < tree.value) return findClosestValueInBstHelper(tree.left, target, closest)
+  if (target > tree.value) return findClosestValueInBstHelper(tree.right, target, closest)
+  if (target === tree.value) {
+    closest === tree.value;
+    return closest;
+  }
+}
 // This is the class of the input tree. Do not edit.
 class BST {
   constructor (value) {
