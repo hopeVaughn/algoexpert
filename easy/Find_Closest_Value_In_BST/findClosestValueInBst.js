d11af86e30d853881be0fb4ab8d8d35be920bbee
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-01 10:59:45 -0800

    add tree structure for bst challenge

--- easy/Find_Closest_Value_In_BST/findClosestValueInBst.js
+++ easy/Find_Closest_Value_In_BST/findClosestValueInBst.js
@@ -19,6 +19,8 @@ target: 12
 
 function findClosestValueInBst(tree, target) {
   // Write your code here.
+  let closest = Infinity;
+
 }
 
 // This is the class of the input tree. Do not edit.
@@ -29,3 +31,15 @@ class BST {
     this.right = null;
   }
 }
+
+const root = new BST(10);
+root.left = new BST(5);
+root.left.left = new BST(2);
+root.left.left.left = new BST(1);
+root.left.right = new BST(5);
+root.right = new BST(15);
+root.right.left = new BST(13);
+root.right.left.right = new BST(14);
+root.right.right = new BST(22);
+
+console.log(findClosestValueInBst(root, 12));
