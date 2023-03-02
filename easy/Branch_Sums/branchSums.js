c7a8b9da972575525fbcd0387f65e49145472c03
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-02 10:42:58 -0800

    add solution for branchSum challenge

--- easy/Branch_Sums/branchSums.js
+++ easy/Branch_Sums/branchSums.js
@@ -34,7 +34,19 @@ class BinaryTree {
 }
 
 function branchSums(root) {
-  console.log(root);
+  let sums = []
+  calculateBranchSums(root, 0, sums)
+  return sums
+}
+
+function calculateBranchSums(node, runningSum, sums) {
+  if (!node) return
+  const newRunningSum = runningSum + node.value;
+  if (!node.left && !node.right) {
+    sums.push(newRunningSum)
+  }
+  calculateBranchSums(node.left, newRunningSum, sums)
+  calculateBranchSums(node.right, newRunningSum, sums)
 }
 
 // extend class to build tree for challenge
