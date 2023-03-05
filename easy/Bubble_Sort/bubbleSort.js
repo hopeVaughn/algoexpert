547e07ac6a12fcd468a1dc4640015649c8b3e88e
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-05 10:56:01 -0800

    adjust syntax

--- easy/Bubble_Sort/bubbleSort.js
+++ easy/Bubble_Sort/bubbleSort.js
@@ -14,7 +14,7 @@ function bubbleSort(array) {
     for (let j = 0; j < i - 1; j++) {
       if (array[j] > array[j + 1]) {
         let temp = array[j];
-        array[j] = array[j + 1]
+        array[j] = array[j + 1];
         array[j + 1] = temp;
         noSwaps = false
       }
