8572b377a94d09caa0f78b45d763581acd88ea21
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-05 10:50:59 -0800

    add bubbleSort challenge/solution/time complex

--- easy/Bubble_Sort/bubbleSort.js
+++ easy/Bubble_Sort/bubbleSort.js
@@ -6,5 +6,25 @@ const array = [8,5,2,9,5,6,3]
 
 Sample Output: [2,3,5,5,6,8,9]
 */
+const array = [8, 5, 2, 9, 5, 6, 3]
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp;
+        noSwaps = false
+      }
+    }
+    if (noSwaps) break
+  }
+  return arr
+}
 
-function 
+console.log(bubbleSort(array));
+
+// time: O(n^2)
+// space: O(1)
